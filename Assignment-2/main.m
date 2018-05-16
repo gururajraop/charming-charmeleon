45e9740a7b8274256bab91ccd9f9128cde7743ee
Gururaja Rao P <gururajraop@gmail.com>
2018-05-16 17:12:28 +0200

    Add stitching procedure

--- Assignment-2/main.m
+++ Assignment-2/main.m
@@ -6,18 +6,19 @@ close all
 data_path = './Data/House/';
 
 %% matching process
-image_matching(data_path);
+% image_matching(data_path);
 
 %% Chaining
-pointviewMatrix = chaining(data_path);
+% point_view_matrix = chaining(data_path);
+% save('./Results/point_view_matrix.mat', 'point_view_matrix');
 
-%%
-% TODO: Get denseBlock from pointviewMatrix, until then use the loaded one
-indices = find(sum(pointviewMatrix(:,:)~=0) == 96);
-denseBlock = pointviewMatrix(:, indices);
-denseBlock = denseBlock(1:96, :);
-
-denseBlock = load('pointviewmatrix.txt');
+point_view_matrix = load('pointviewmatrix.txt');
 %% Structure from motion
-sfm(denseBlock)
+% sfm(denseBlock, true)
+
+%% Build 3D structure from point view matrix
+sample_size = 200;
+step_size = 4;
+% load('./Results/point_view_matrix.mat');
+plot_3D_structure(point_view_matrix, step_size, sample_size);
 
--- Assignment-2/sfm.m
+++ Assignment-2/sfm.m
@@ -1,4 +1,4 @@
-function sfm(block)
+function [M, S] = sfm(block, visualize)
     % Center each view in the dense block
     block = block - mean(block, 2);
 
@@ -8,14 +8,16 @@ function sfm(block)
     % Use first three singular values
     U = U(:, 1:3);
     W = W(1:3, 1:3);
-    V = V(:, 1:3); 
+    V = V(:, 1:3);
 
     % Factorize into M and S
     M = U * sqrtm(W);
     S = sqrtm(W) * V';
 
     % Visualize
-    figure()
-    fscatter3(S(1,:), S(2,:), S(3,:), S(1,:));
+    if visualize
+        figure()
+        fscatter3(S(1,:), S(2,:), S(3,:), S(1,:));
+    end
 end
 
